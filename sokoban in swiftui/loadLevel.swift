0998048fcca08c90ce34cb949180101bdf1dd91b
sbohmann <mail@sebastian-bohmann.com>
2023-06-29 20:40:30 +0200

    Refactoring of level parsing

--- sokoban in swiftui/loadLevel.swift
+++ sokoban in swiftui/loadLevel.swift
@@ -11,52 +11,66 @@ func loadLevel(_ number: Int) -> LevelData {
     var map = [Field]()
     var boxes = [Position]()
     var player: Position?
-    for y in 0 ..< height {
-        let line = lines[y]
-        for x in 0 ..< width {
-            if x < line.count {
-                let character = line[x]
-                switch character {
-                case " ":
-                    map.append(.open)
-                case ".":
-                    map.append(.target)
-                case "#":
-                    map.append(.wall)
-                case "@":
-                    map.append(.open)
-                    if player != nil {
-                        fatalError("Multiple players")
-                    }
-                    player = Position(x: x, y: y)
-                case "+":
-                    map.append(.target)
-                    if player != nil {
-                        fatalError("Multiple players")
-                    }
-                    player = Position(x: x, y: y)
-                case "$":
-                    map.append(.open)
-                    boxes.append(Position(x: x, y: y))
-                case "*":
-                    map.append(.target)
-                    boxes.append(Position(x: x, y: y))
-                default:
-                    fatalError("Illegal character [\(character)]")
-                }
-            } else {
-                map.append(.open)
-            }
-        }
-    }
+    
+    parseMap()
     
     if (map.count != width * height) {
         fatalError("map size is off")
     }
     
-    return LevelData(width:width,
-                     height: height,
-                     map: map,
-                     boxes: boxes,
-                     player: player!)
+    func parseMap() {
+        for y in 0 ..< height {
+            let line = lines[y]
+            for x in 0 ..< width {
+                parseField(line: line, x: x, y: y)
+            }
+        }
+    }
+    
+    func parseField(line: [Unicode.Scalar], x: Int, y: Int) {
+        if x < line.count {
+            parseFieldCharacter(character: line[x], x: x, y: y)
+        } else {
+            map.append(.open)
+        }
+    }
+    
+    func parseFieldCharacter(character: Unicode.Scalar, x: Int, y: Int) {
+        switch character {
+        case " ":
+            map.append(.open)
+        case ".":
+            map.append(.target)
+        case "#":
+            map.append(.wall)
+        case "@":
+            map.append(.open)
+            setPlayer(x, y)
+        case "+":
+            map.append(.target)
+            setPlayer(x, y)
+        case "$":
+            map.append(.open)
+            boxes.append(Position(x: x, y: y))
+        case "*":
+            map.append(.target)
+            boxes.append(Position(x: x, y: y))
+        default:
+            fatalError("Illegal character [\(character)]")
+        }
+    }
+    
+    func setPlayer(_ x: Int, _ y: Int) {
+        if player != nil {
+            fatalError("Multiple players")
+        }
+        player = Position(x: x, y: y)
+    }
+    
+    return LevelData(
+        width:width,
+        height: height,
+        map: map,
+        boxes: boxes,
+        player: player!)
 }
