e21a3dea941b00717a3df2994098ae3dc80f7925
sbohmann <github@yeoman.at>
2023-08-03 18:26:47 +0200

    Level import

--- level_import/import.js
+++ level_import/import.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 
+console.log(process.argv)
+
 if (process.argv.length !== 3) {
     throw RangeError("Expecting one argument: <path to input file>")
 }
@@ -8,7 +10,12 @@ const inputFile = process.argv[2]
 
 if (!fs.existsSync(inputFile) || !fs.statSync(inputFile).isFile()) {
     throw RangeError("Not a file: [" + inputFile + "]")
-}``
+}
 
-fs.readFileSync('./')
-    .forEach(console.log)
+fs.readFileSync(inputFile, 'utf-8')
+    .split(/\r?\n/)
+    .forEach(parseLine)
+
+function parseLine(line) {
+    console.log(line)
+}
